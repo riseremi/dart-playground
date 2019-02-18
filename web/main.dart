6b02816449a1a5a7528ca960c5fca7fd8aa397d1
riseremi <riseremi@icloud.com>
2019-02-18 03:41:17 +0300

    Reduce code duplication

--- web/main.dart
+++ web/main.dart
@@ -1,15 +1,18 @@
 import 'dart:html' as html;
 
-void main() {
+void main() async {
   var canvas = html.CanvasElement(width: 128, height: 128);
+  var game = html.querySelector("#game");
 
   // Workaround for hot-reloading.
-  html.querySelector('#game').innerHtml = '';
+  game.innerHtml = '';
 
-  html.querySelector('#game').append(canvas);
+  game.append(canvas);
 
   var context = canvas.context2D;
 
   context.fillStyle = '#4a4a4a';
   context.fillRect(0, 0, 128, 128);
+  context.fillStyle = '#90f';
+  context.fillRect(16, 16, 128 - 16 * 2, 128 - 16 * 2);
 }
